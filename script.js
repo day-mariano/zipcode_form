9c1df489e2aa0cc20e9f7ebd2986d7b38c756321
Dayana Mariano <dayanaraquelle@gmail.com>
2022-06-23 23:59:44 -0300

    Add comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cep = document.getElementById("cep");
-
+//Verifica se contém o mesmo campo no doc e add o valor nele
 const showData = (result) => {
   for (const campo in result) {
     if (document.querySelector("#" + campo)) {
@@ -7,15 +7,19 @@ const showData = (result) => {
     }
   }
 };
-
+// Quando tira o foco input cep faz a consulta por fetch
 cep.addEventListener("blur", (e) => {
+  //tratamento do número
   let search = cep.value.replace("-", "");
+
+  // Caracteristicas da requisição
   const options = {
     method: "GET",
     mode: "cors",
     cache: "default",
   };
 
+  // Requisição pelo BrasilApi e chama showData
   fetch(`https://brasilapi.com.br/api/cep/v1/${search}`, options)
     .then((response) => {
       response.json().then((data) => showData(data));
