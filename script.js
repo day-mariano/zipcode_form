ecd1310f45bd9b5bab0d024bee2af2d91245506c
Dayana Mariano <dayanaraquelle@gmail.com>
2022-06-25 17:02:38 -0300

    Add error message

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cep = document.getElementById("cep");
-const fomr = document.getElementById("form")
+const form = document.getElementById("form")
 
 //Verifica se contém o mesmo campo no doc e add o valor nele
 const showData = (result) => {
@@ -25,11 +25,23 @@ cep.addEventListener("blur", (e) => {
   // Requisição pelo BrasilApi e chama showData
   fetch(`https://brasilapi.com.br/api/cep/v1/${search}`, options)
     .then((response) => {
+      if (response.status == 404) {
+        document.getElementById("cepSmall").innerText = "Não encontrado"
+        return
+      } else if (response.status >= 400) {
+        document.getElementById("cepSmall").innerText = "Erro"
+        return
+      }
+      
       response.json().then((data) => showData(data));
+      
     })
-    .catch((e) => console.log(e));
+    .catch((e) => {
+      document.getElementById("cepSmall").innerText = "Erro"
+    });
 });
 
+// Evitar ação padrão do submit: enter para enviar 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 });
